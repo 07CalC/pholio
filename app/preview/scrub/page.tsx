1804d4ae28175745de0516dc34ad971609d1ebf7
CalC <maheshwarivinayak90@gmail.com>
2024-12-01 18:22:22 +0530

    started scrub template

---
+++ app/preview/scrub/page.tsx
@@ -0,0 +1,10 @@
+ 
+
+export default function Home () {
+
+  return (
+  <div className="bg-gradient-to-br text-white text-3xl font-bold flex flex-col p-10 items-center align-center from-[#404241] via-[#181a19] to-[#2c302d] h-svh">
+      hello
+  </div>
+  )
+}
