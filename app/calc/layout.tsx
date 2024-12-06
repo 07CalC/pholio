dfcc1e4745bc7141aee3da7a585e1d5a26275ba1
CalC <maheshwarivinayak90@gmail.com>
2024-12-06 21:49:10 +0530

    bug fixes

--- app/calc/layout.tsx
+++ app/calc/layout.tsx
@@ -1,22 +1,13 @@
 import { Metadata } from "next";
 
-import { Yuji_Mai } from "next/font/google";
-import { Exo_2 } from "next/font/google";
+
 
 export const metadata: Metadata = {
     title: "CalC",
     description: "Hi, this is CalC",
 }
 
-const YujiMai = Yuji_Mai({
-    weight: "400",
-    subsets: ["latin"],
-})
 
-const Exo2 = Exo_2({
-    weight: "400",
-    subsets: ["latin"],
-})
 
 export default function RootLayout({
     children,
@@ -26,7 +17,7 @@ export default function RootLayout({
     return (
       <html lang="en">
         <body
-          className={`bg-[#1f1e1e] ${Exo2.style}`}
+          className={`bg-[#1f1e1e] `}
         >
           {children}
         </body>
