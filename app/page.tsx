53cace3de79ede664cd67a3a30933876709abe2c
CalC <maheshwarivinayak90@gmail.com>
2024-12-12 20:43:22 +0530

    hero page change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
-import { LampContainer } from "@/app/components/ui/lamp";
+
  
 export default function Home() {
   return (
