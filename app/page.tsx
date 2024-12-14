0a07c0e707ee4a8a58ff029a6bab35832356b470
CalC <maheshwarivinayak90@gmail.com>
2024-12-14 16:50:35 +0530

    added info in calc portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
 "use client";
 import React from "react";
-
+import { motion } from "framer-motion";
+import { LampContainer } from "@/app/components/ui/lamp";
  
 export default function Home() {
   return (
-    <div>
-      Dev
-    </div>
-
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Build Portfolio in Minutes 
+      </motion.h1>
+      <motion.h3
+      initial={{ opacity: 0.5, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+      className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        coming soon
+      </motion.h3>
+    </LampContainer>
   );
 }
