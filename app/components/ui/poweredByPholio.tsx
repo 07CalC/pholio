43ed259f624dbd555e9ca0f0acb56f4bfe918e26
CalC <maheshwarivinayak90@gmail.com>
2024-12-15 20:12:29 +0530

    fixed pholio link

--- app/components/ui/poweredByPholio.tsx
+++ app/components/ui/poweredByPholio.tsx
@@ -6,13 +6,14 @@ export const PoweredByPholio = () => {
         className={`fixed gap-x-1 flex font-semibold font-mono box-decoration-slice drop-shadow-xl self-center md:self-start py-2 text-sm text-end text-black md:ml-5 bottom-0 mb-5 bg-gradient-to-br from-[#9df89f] to-[#4b67da] rounded-xl px-2 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_5px_#08f,0_0_10px_#08f] hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_25px_#08f]`}
       >
         <a
-      className=" md:self-start"
+      className="flex gap-x-1 md:self-start"
       href="https://pholio.batua.site/"
       target="_blank"
       rel="noopener noreferrer"
-    ></a>
+    >
         <img src="../favicon.ico" alt="logo" className="w-5 h-5" />
         <p>powered by Pholio</p>
+        </a>
       </div>
     
     
