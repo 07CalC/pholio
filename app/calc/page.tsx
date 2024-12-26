75deaa040e98d9b72fad1db8ac29bd9007e18938
CalC <96346957+07CalC@users.noreply.github.com>
2024-12-26 21:59:49 +0530

    Update page.tsx

--- app/calc/page.tsx
+++ app/calc/page.tsx
@@ -1,27 +1,39 @@
 
 import { Navbar } from "./components/Navbar";
-import { Space_Mono } from "next/font/google";
+
 import { PoweredByPholio } from "../components/ui/poweredByPholio";
+import { Info } from "./components/Info";
 
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
+import { recursive } from "../fonts/Fonts";
 
-const SpaceMono = Space_Mono({
-  weight: "400",
-  subsets: ["latin"],
-});
 
 export default function Calc() {
   return (
     <div
-      className={`bg-[#1f1e1e] min-h-svh w-full text-white text-3xl font-bold flex flex-col items-center align-center h-svh ${SpaceMono.className}`}
+      className={`${recursive.className} bg-[#1b1b1b] bg-[linear-gradient(to_right,#505050_1px,transparent_1px),linear-gradient(to_bottom,#505050_1px,transparent_1px)] bg-[size:60px_60px] pb-16 min-h-svh h-full w-full text-white text-3xl font-bold flex flex-col items-center  align-center`}
     >
-      
-      <div className="w-1/3 fixed mt-5">
+      <div className="lg:w-1/2 w-11/12 fixed mt-3 lg:mt-5">
         <Navbar />
       </div>
-      <div className="w-full h-full"></div>
+      <div className="w-full h-20">
+      </div>
+
+      <div className="w-full ">
+        <Info />
+      </div>
+      
+      <div className="w-full">
+          <Skills />
+        </div>
+
+      <div className="w-full mt-16">
+        < Projects />
+      </div>
 
       
-      
+       
         <PoweredByPholio />
       
     </div>
