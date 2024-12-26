1a113403843046dd61edfe7d4948ba51c2a799e3
CalC <96346957+07CalC@users.noreply.github.com>
2024-12-26 21:55:05 +0530

    added Fonts.ts

---
+++ app/fonts/Fonts.ts
@@ -0,0 +1,36 @@
+import { Press_Start_2P } from "next/font/google";
+import { Jaro } from "next/font/google";
+import { Ysabeau_SC } from "next/font/google";
+import { Finger_Paint } from "next/font/google";
+import { Rubik_Vinyl } from "next/font/google";
+import { Recursive } from "next/font/google";
+
+export const pressStart2P = Press_Start_2P({
+    weight: "400",
+    subsets: ["latin"],
+});
+
+export const jaro = Jaro({
+    weight: "400",
+    subsets: ["latin"],
+});
+
+export const fingerPaint = Finger_Paint({
+    weight: "400",
+    subsets: ["latin"],
+});
+
+export const rubikVinyl = Rubik_Vinyl({
+    weight: "400",
+    subsets: ["latin"],
+})
+
+export const ysabeauSc = Ysabeau_SC({
+    weight: "400",
+    subsets: ["latin"],
+});
+
+export const recursive = Recursive({
+    weight: "400",
+    subsets: ["latin"],
+});
